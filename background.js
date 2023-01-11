19135eef8bd96585f7091a8813cbcab558f0e187
yuyu-10 <104772989+yuyu-10@users.noreply.github.com>
2023-01-11 16:18:37 +0100

    v1 complete, with definition, synonyms with the recherche input

--- background.js
+++ background.js
@@ -1,59 +1,6 @@
-const bouton = document.getElementById("btn");
-let word = document.getElementById("theWord");
-const theDef = document.getElementById("def");
-const theSyn = document.getElementById("syn")
+// document.addEventListenner("mouseup", selectWord);
 
-function displayDefinition() {
-    theDef.style.display = "flex"
-    theDef.style.padding = "20px"
-    displayWord();
+function selectWord() {
+    let selectText = window.getSelection().toString();
+    console.log(selectText);
 }
-
-function displayBtn() {
-    word = document.getElementById("theWord").value
-    bouton.style.display = "flex";
-
-}
-
-function displayWord() {
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-        .then(data => data.json())
-        .then(data => theDef.innerHTML = data[0].meanings[0].definitions[0].definition)
-}
-
-function fetchSyn() {
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-        .then(data => data.json())
-        .then(data => displaySyn(data))
-}
-
-function displaySyn(result) {
-    let lengthOfSynonyms = result[0].meanings[0].synonyms.length
-    theSyn.innerHTML = " "
-    if (lengthOfSynonyms === 0) {
-        theSyn.innerHTML = "Sorry, there is no synonym..."
-    } else {
-        for (i = 0; i < lengthOfSynonyms; i++) {
-            if (result[0].meanings[0].synonyms[i] !== "") {
-                theSyn.innerHTML += `<li>${result[0].meanings[0].synonyms[i]}</li>` + "<br>"
-            } else {
-                theSyn.innerHTML = "Sorry, there is no synonym..."
-            }
-        }
-    }
-}
-
-function displaySynonyms() {
-    theSyn.style.display = "flex"
-    theSyn.style.padding = "20px"
-    fetchSyn()
-}
-
-const send = document.getElementById("valid");
-send.addEventListener("click", displayBtn);
-
-const defDescription = document.getElementById('defBtn');
-defDescription.addEventListener("click", displayDefinition);
-
-const synDescription = document.getElementById('synBtn');
-synDescription.addEventListener("click", displaySynonyms);
---
+++ content.js
@@ -0,0 +1,60 @@
+const bouton = document.getElementById("btn");
+let word = document.getElementById("theWord");
+const theDef = document.getElementById("def");
+const theSyn = document.getElementById("syn");
+
+function displayBtn() {
+
+    word = document.getElementById("theWord").value
+    bouton.style.display = "flex";
+
+}
+
+function displayWord() {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        .then(data => data.json())
+        .then(data => theDef.innerHTML = data[0].meanings[0].definitions[0].definition)
+}
+
+function fetchSyn() {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        .then(data => data.json())
+        .then(data => displaySyn(data))
+}
+
+function displaySyn(result) {
+    let lengthOfSynonyms = result[0].meanings[0].synonyms.length
+    theSyn.innerHTML = " "
+    if (lengthOfSynonyms === 0) {
+        theSyn.innerHTML = "Sorry, there is no synonym..."
+    } else {
+        for (i = 0; i < lengthOfSynonyms; i++) {
+            if (result[0].meanings[0].synonyms[i] !== "") {
+                theSyn.innerHTML += `<li>${result[0].meanings[0].synonyms[i]}</li>` + "<br>"
+            } else {
+                theSyn.innerHTML = "Sorry, there is no synonym..."
+            }
+        }
+    }
+}
+
+function displaySynonyms() {
+    theSyn.style.display = "flex"
+    theSyn.style.padding = "20px"
+    fetchSyn()
+}
+
+function displayDefinition() {
+    theDef.style.display = "flex"
+    theDef.style.padding = "20px"
+    displayWord();
+}
+
+const send = document.getElementById("valid");
+send.addEventListener("click", displayBtn);
+
+const defDescription = document.getElementById('defBtn');
+defDescription.addEventListener("click", displayDefinition);
+
+const synDescription = document.getElementById('synBtn');
+synDescription.addEventListener("click", displaySynonyms);
